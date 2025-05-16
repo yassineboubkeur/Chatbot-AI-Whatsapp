abe336288c0902cb7de029420b298620fe7a75b1
Coderyounes <younesanas2021@gmail.com>
2025-05-16 16:15:45 +0100

    Add Flask application factory and SQLAlchemy setup

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config')
+    db.init_app(app)
+
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
