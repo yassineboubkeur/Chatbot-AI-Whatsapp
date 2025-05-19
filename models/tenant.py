6970f19839504e7f3e5709d261b893898e23543f
Coderyounes <younesanas2021@gmail.com>
2025-05-19 16:54:52 +0100

    Add Tenant model to define tenant attributes and database schema

---
+++ models/tenant.py
@@ -0,0 +1,12 @@
+from app import db
+
+
+class Tenant(db.Model):
+    __tablename__ = 'tenants'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(20))
+    phone_number_id = db.Column(db.String(20))
+    whatsapp_token = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
