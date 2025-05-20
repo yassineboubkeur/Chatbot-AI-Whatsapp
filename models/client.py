cf391594037d0e29a06beea4e41aabe44f0eef77
Coderyounes <younesanas2021@gmail.com>
2025-05-20 10:23:32 +0100

    Add Client model with fields for fullname, phone number, and timestamps

---
+++ models/client.py
@@ -0,0 +1,13 @@
+from app import db
+
+
+class Client(db.Model):
+    __tablename__ = 'clients'
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+
+    tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'), nullable=False)
+
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
