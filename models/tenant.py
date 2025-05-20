7a912f65d1b21118b566f57c0e5cac160aeb1c52
Coderyounes <younesanas2021@gmail.com>
2025-05-20 10:22:20 +0100

    Add relationships to Tenant model for associated entities

--- models/tenant.py
+++ models/tenant.py
@@ -10,3 +10,8 @@ class Tenant(db.Model):
     whatsapp_token = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    tenants_info = db.relationship('TenantInfo', backref='tenants', lazy=True, uselist=False)
+    clients = db.relationship('Client', backref='clients', lazy=True)
+    services = db.relationship('Service', backref='services', lazy=True)
+    products = db.relationship('Product', backref='products', lazy=True)
