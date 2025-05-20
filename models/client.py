c34ea9918690ec448c5e7cefe92371a4e0efd74d
Coderyounes <younesanas2021@gmail.com>
2025-05-20 15:04:31 +0100

    Make phone_number column unique in clients table

--- models/client.py
+++ models/client.py
@@ -1,3 +1,5 @@
+from enum import unique
+
 from app import db
 
 
@@ -5,7 +7,7 @@ class Client(db.Model):
     __tablename__ = 'clients'
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(100), nullable=False)
-    phone_number = db.Column(db.String(20), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False, unique=True)
 
     tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'), nullable=False)
 
