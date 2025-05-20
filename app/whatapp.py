b7dabfbbe9562abe540ca87147b597b51c77202a
Coderyounes <younesanas2021@gmail.com>
2025-05-20 11:25:08 +0100

    Add function to extract client access token from Tenant model

--- app/whatapp.py
+++ app/whatapp.py
@@ -1,6 +1,21 @@
 import requests
 from config import WHATSAPP_TOKEN, PHONE_NUMBER_ID
+from models.tenant import Tenant
 
+
+
+def extract_client_access_token(phone):
+    tenantObj = Tenant.query.filter_by(phone=phone).first()
+    if tenantObj:
+        whatsapp_token = tenantObj.whatsapp_token
+        phone_number_id = tenantObj.phone_number_id
+    else:
+        whatsapp_token = None
+        phone_number_id = None
+    return whatsapp_token, phone_number_id
+
+
+# TODO: add from parameter contain the tenant phone number so we can dynamically get his whatsapp token, phone number id
 def send_message(to, message):
     url  = f"https://graph.facebook.com/v17.0/{PHONE_NUMBER_ID}/messages"
 
@@ -21,4 +36,6 @@ def send_message(to, message):
         return True
     except requests.exceptions.RequestException as e:
         print(">>> Exception while sending message: ", e)
+        if hasattr(e, 'response') and e.response:
+            print(">>> Response: ", e.response.text)
         return False
