382bbbc8990d0ccd96f0cd249a7fe0d269945a92
Coderyounes <younesanas2021@gmail.com>
2025-05-20 11:25:25 +0100

    Make phone_number field in Tenant model unique and indexed

--- models/tenant.py
+++ models/tenant.py
@@ -5,7 +5,7 @@ class Tenant(db.Model):
     __tablename__ = 'tenants'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    phone_number = db.Column(db.String(20))
+    phone_number = db.Column(db.String(20), index=True, unique=True)
     phone_number_id = db.Column(db.String(20))
     whatsapp_token = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
