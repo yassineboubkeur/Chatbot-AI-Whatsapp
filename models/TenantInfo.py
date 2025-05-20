635aec09f53096b4ba2281039a721985eee14209
Coderyounes <younesanas2021@gmail.com>
2025-05-20 10:22:08 +0100

    Add TenantInfo model to define tenant attributes and database schema

---
+++ models/TenantInfo.py
@@ -0,0 +1,15 @@
+from enum import unique
+
+from app import db
+
+
+class TenantInfo(db.Model):
+    __tablename__ = 'tenant_info'
+    id = db.Column(db.Integer, primary_key=True)
+    tenant_name = db.Column(db.String(100), nullable=False)
+    tenant_email = db.Column(db.String(100), nullable=False)
+    tenant_phone_number = db.Column(db.String(20), nullable=False)
+
+    tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
