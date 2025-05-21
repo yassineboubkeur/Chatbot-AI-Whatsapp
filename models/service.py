086d893c1eb508aadad8ff2cf4e8f5199a1517d6
Coderyounes <younesanas2021@gmail.com>
2025-05-21 12:21:07 +0100

    Add embedding field and method to generate and save embeddings in Service model

--- models/service.py
+++ models/service.py
@@ -1,6 +1,8 @@
+from openai import OpenAI
+
 from app import db
-
-
+from pgvector.sqlalchemy import VECTOR
+from config import OPENAI_API_KEY
 class Service(db.Model):
     __tablename__ = 'services'
     id = db.Column(db.Integer, primary_key=True)
@@ -11,4 +13,23 @@ class Service(db.Model):
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
+    embedding = db.Column(VECTOR(1536), nullable=True)
+
     tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'), nullable=False)
+
+
+    def generate_and_save_embedding(self):
+        text = f"Offer: {self.name} {self.description} {self.price} {self.periode}"
+
+        client = OpenAI(api_key=OPENAI_API_KEY)
+
+        response = client.embeddings.create(
+            input=text,
+            model="text-embedding-ada-002",
+        )
+
+        self.embedding = response.data[0].embedding
+
+        db.session.commit()
+
+        return self.embedding
