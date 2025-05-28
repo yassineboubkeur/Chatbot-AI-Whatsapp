e68f70fc68c8fbe613f227490ee4366acb075f0f
Coderyounes <younesanas2021@gmail.com>
2025-05-28 09:47:41 +0100

    Update run.py to enable dynamic debug mode and configurable port

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    # TODO: turn this into False when deploying to production
-    app.run(debug=True)
+    debug_mode = os.getenv('DEBUG', 'False').lower() == 'true'
+    app.run(debug=debug_mode, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
